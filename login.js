6eaa8bf00a1ebb1e01b990f2fa7004802682e6e0
rsinema <rileysinema@gmail.com>
2023-03-10 17:33:19 -0700

    fixed login.js to grab username input box

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function login() {
-    const nameEl = document.querySelector("#name");
+    const nameEl = document.querySelector("#username");
     localStorage.setItem("userName", nameEl.value);
     window.location.href = "pong.html";
 }
